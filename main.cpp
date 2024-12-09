02eea804a64791e3a8c4551e52c86ae4366813f0
cheryxso <stor8ge18@gmail.com>
2024-12-09 04:55:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "FuncA.h"
+#include "HttpServer.h"
 
 int main() {
-  FuncA func;
-  std::cout << "FuncA result: " << func.calculate(1.0, 10) << std::endl;
-  return 0;
+    FuncA func;
+    std::cout << "FuncA result (test): " << func.calculate(1.0, 10) << std::endl;
+
+    HttpServer server;
+    server.start();
+
+    return 0;
 }
+
