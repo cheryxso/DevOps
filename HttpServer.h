ba216c8c6949c7f42c51d7deaaffb3c27237afe6
cheryxso <stor8ge18@gmail.com>
2024-12-12 15:11:14 +0200

    Update HttpServer.h

--- HttpServer.h
+++ HttpServer.h
@@ -2,17 +2,14 @@
 #define HTTPSERVER_H
 
 #include <string>
-#include <vector>
 
 class HttpServer {
 public:
     void start();
 
 private:
-    void handleClient(int client_socket);
-    std::vector<double> prepareAndSortData(int n);
-    std::string handleComputeRequest(int n);
-    int extractParameter(const std::string& request, const std::string& param, int default_value);
+    void handleClient(int client_fd);
+    std::string handleCalculate(double x);
 };
 
 #endif
